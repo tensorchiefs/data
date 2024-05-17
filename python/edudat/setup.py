a8d47ecbf6b28948a73993ae0fb800d445a7ed3e
Oliver Dürr <oliver.duerr@gmail.com>
2024-05-17 11:21:25 +0200

    added setup

---
+++ python/edudat/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="edudat",
+    version="0.001",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "requests",
+    ],
+    author="Oliver Dürr",
+    description="A package for educational datasets",
+    url="https://github.com/oduerr/data",
+)
