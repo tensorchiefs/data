1c5adb42d26db45c7f52a578fbf2f5fbdf39f546
Oliver Dürr <oliver.duerr@gmail.com>
2024-05-18 17:56:30 +0200

    checked in stuff

--- R/edudat/R/internal.R
+++ R/edudat/R/internal.R
@@ -9,7 +9,7 @@ execute_quarto_chunk <- function(file, chunk_name) {
     if (grepl(pattern, line)){
       in_chunk <- TRUE
       next
-    }
+    } 
     if (in_chunk && grepl("```", line, fixed = TRUE)) {
       in_chunk <- FALSE
       break
@@ -26,3 +26,23 @@ execute_quarto_chunk <- function(file, chunk_name) {
     message("Chunk not found: ", chunk_name)
   }
 }
+
+
+#' Rendering the information provided in the Quarto document
+#' 
+#' @param df A data frame containing the dataset
+#' @examples
+#' df <- load_data("challenger.csv")
+#' render_info(df)
+# info_data <- function(df) {
+#   # Get the name of the dataset from the attribute
+#   dataset_name <- attr(df, "dataset_name")
+#   quarto_file = load_quarto_file(paste0(dataset_name, ".qmd")) 
+#   library(quarto)
+#   quarto::(quarto_file)
+#   if (file.exists(quarto_file)) {
+#     execute_quarto_chunk(quarto_file, "info")
+#   } else {
+#     stop("Quarto document not found: ", quarto_file)
+#   }
+# }
